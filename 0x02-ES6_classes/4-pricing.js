eb9a0e31c90dbe33f137adb36ddddabd6773ee3d
Deey-Kerry <dianahkerrynyash@gmail.com>
2024-03-07 20:42:15 +0000

    0x02-ES6_classes

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,14 +1,5 @@
-import Currency from './3-currency';
-
 export default class Pricing {
   constructor(amount, currency) {
-    if (typeof amount !== 'number') {
-      throw new TypeError('Amount must be a number');
-    }
-    if (!(currency instanceof Currency)) {
-      throw new TypeError('Currency must be a Currency instance');
-    }
-
     this._amount = amount;
     this._currency = currency;
   }
@@ -17,31 +8,23 @@ export default class Pricing {
     return this._amount;
   }
 
-  set amount(amount) {
-    if (typeof amount !== 'number') {
-      throw new TypeError('Amount must be a number');
-    }
-    this._amount = amount;
+  set amount(a) {
+    this._amount = a;
   }
 
   get currency() {
     return this._currency;
   }
 
-  set currency(currency) {
-    if (!(currency instanceof Currency)) {
-      throw new TypeError('Currency must be a Currency instance');
-    }
-    this._currency = currency;
+  set currency(c) {
+    this._currency = c;
   }
 
-  
   displayFullPrice() {
-    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+    return `${this.amount} ${this.currency._name} (${this.currency._code})`;
   }
 
-  
   static convertPrice(amount, conversionRate) {
-    return amount * conversionRate;
+    return (amount * conversionRate);
   }
 }
